4cd0bc355893ed385cb9e674eb6d57c6e52699b4
rapid <cse.juneo@gmail.com>
2022-07-02 21:49:41 +0900

    자료구조 (10828)

--- BaekJoon-Algorithm/자료구조/main.py
+++ BaekJoon-Algorithm/자료구조/main.py
@@ -7,6 +7,27 @@
 # empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
 # top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 
+import sys
+
+stack = []
+count = int(input())
+
+for i in range (count):
+    list = sys.stdin.readline().split()
+    command = list[0]
+    if command == "push":
+        stack.append(int(list[1]))
+    elif command == "pop":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif command == "size":
+        print(len(stack))
+    elif command == "empty":
+        print(1 if len(stack) == 0 else 0)
+    elif command == "top":
+        print(stack[-1] if len(stack) > 0 else -1)
 
 
 
